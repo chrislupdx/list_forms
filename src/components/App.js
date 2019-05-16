14a9fd10c82b766d5823bad13d7882803b25295b
dreamer <wildetangeant@gmail.com>
2019-05-16 14:12:45 -0700

    bad error message working

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ErrorBoundary from './ErrorBoundary';
-
+import Bad from './Bad';
 export default function App() {
   return (
         <>
+            <ErrorBoundary>
+              <Bad />
+            </ErrorBoundary>
         </>
   );
 }
