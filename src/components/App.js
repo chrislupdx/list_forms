9adc13b6c548f7fb68611f51eafd65956d03be1e
dreamer <wildetangeant@gmail.com>
2019-05-16 13:49:46 -0700

    app test passed

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import Colors from './Colors';
 
 export default function App() {
-  const colors = [
-    { name: 'red', rgb: { red: 255, green: 0, blue: 0 } },
-    { name: 'green', rgb: { red: 0, green: 255, blue: 0 } },
-    { name: 'blue', rgb: { red: 0, green: 0, blue: 233 } }
-  ];
-  return <Colors colors={colors} />;
+  return (
+        <>
+        </>
+  );
 }
-
